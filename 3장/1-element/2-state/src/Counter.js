7dd47bdcd22ea60a7062e48ddb48de8de57c7221
songks0922 <skj0922@kookmin.ac.kr>
2021-08-31 14:38:34 +0900

    createPortal 실습

--- 3장/1-element/2-state/src/Counter.js
+++ 3장/1-element/2-state/src/Counter.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import Title from './Title';
 
-export default function Counter({ title }) {
-  const [count, setCount] = useState(0);
+export default function Counter() {
+  const [count, setCount] = useState({ value: 0 });
   function onClick() {
-    setCount(count + 1);
+    setCount({ ...count, value: count.value + 1 });
   }
   return (
     <div>
-      <Title title={`현재 카운트: ${count}`} />
+      <Title title={`현재 카운트: ${count.value}`} />
       <button onClick={onClick}>증가</button>
     </div>
   );
--- 3장/1-element/2-state/src/index.js
+++ 3장/1-element/2-state/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Counter from './Counter';
 
 ReactDOM.render(
   <React.StrictMode>
