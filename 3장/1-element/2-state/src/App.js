dd555e70840a164510e584594aff249553cc357d
songks0922 <skj0922@kookmin.ac.kr>
2021-08-31 10:13:42 +0900

    refactor: 필요없는 파일 삭제

--- 3장/1-element/2-state/src/App.js
+++ 3장/1-element/2-state/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 
 export default function App() {
   const [todoList, setTodoList] = useState([]);
@@ -7,13 +6,13 @@ export default function App() {
   const [desc, setDesc] = useState('');
   const [showOdd, setshowOdd] = useState(false);
   function onAdd() {
-    const todo = {id: currentId, desc};
-    setCurrentID(currentId+1);
+    const todo = { id: currentId, desc };
+    setCurrentID(currentId + 1);
     setTodoList([...todoList, todo]);
   }
   function onDelete(e) {
     const id = Number(e.target.dataset.id);
-    const newTodoList = todoList.filter(todo => todo.id !== id);
+    const newTodoList = todoList.filter((todo) => todo.id !== id);
     setTodoList(newTodoList);
   }
   function onServeToServer() {}
@@ -22,15 +21,24 @@ export default function App() {
     <div>
       <h3>할 일 목록</h3>
       <ul>
-        {todoList.filter((_, index) => showOdd ? index % 2 === 0 : true)
-        .map(todo => (
-          <li key={todo.id}>
-            <span>{todo.desc}</span>
-            <button data-id={todo.id} onClick={onDelete}>삭제</button>
-          </li>
-        ))}
+        {todoList
+          .filter((_, index) => (showOdd ? index % 2 === 0 : true))
+          .map((todo) => (
+            <li key={todo.id}>
+              <span>{todo.desc}</span>
+              <button data-id={todo.id} onClick={onDelete}>
+                삭제
+              </button>
+            </li>
+          ))}
       </ul>
-      <input type="text" value={desc} onChange={e => {setDesc(e.target.value)}} />
+      <input
+        type="text"
+        value={desc}
+        onChange={(e) => {
+          setDesc(e.target.value);
+        }}
+      />
       <button onClick={onAdd}>추가</button>
       <button onClick={() => setshowOdd(!showOdd)}>
         홀수 아이템만 보기 on/off
