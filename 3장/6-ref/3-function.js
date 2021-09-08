9e3715e2d4225dbfc5e4f50894b0a029783aaf0c
songks0922 <skj0922@kookmin.ac.kr>
2021-09-08 19:13:57 +0900

    feat: ref를 이용하는 실습 코드

---
+++ 3장/6-ref/3-function.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  const [text, setText] = useState(INITIAL_TEXT);
+  const [showText, setShowText] = useState(true);
+  return (
+    <div>
+      {showText && (
+        <input
+          type="text"
+          ref={(ref) => ref && setText(INITIAL_TEXT)}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      )}
+      <button onClick={() => setShowText(!showText)}>보이기/가리기</button>
+    </div>
+  );
+}
+
+const INITIAL_TEXT = '안녕하세요';
