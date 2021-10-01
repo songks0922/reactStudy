e029557df4ab3198b94709a3a1768f5ca01d40db
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-10-01 14:38:55 +0900

    feat: 6장 실습예제-1

---
+++ 6장/2-element/3.js
@@ -0,0 +1,13 @@
+export const ADD = 'todo/ADD';
+export const REMOVE = 'todo/REMOVE';
+export const REMOVE_ALL = 'todo/REMOVE_ALL';
+
+function addTodo({ title, priority }) {
+  return { type: ADD, title, priority };
+}
+function removeTodo({ id }) {
+  return { type: REMOVE, id };
+}
+function removeAllTodo() {
+  return { type: REMOVE_ALL };
+}
